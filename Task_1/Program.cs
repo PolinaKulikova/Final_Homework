7ddbea0d2057156b2d8edcc4603c30cb6c078ffb
Polina Kulikova <pomidorishna@gmail.com>
2023-01-21 21:03:38 +0300

    Initual commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.WriteLine("Введите число");
+int N =  Convert.ToInt32(Console.ReadLine());
+
+// int N = 5;
+int index = N;
+while (index > 0)
+{
+    Console.WriteLine(index);
+    index = index - 1;
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+// int M = 1;
+// int N = 15;
+int sum = 0;
+int index = M;
+while (index <= N)
+{
+    sum = sum + index;
+    index++;
+}
+Console.WriteLine(sum);
